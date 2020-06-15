9e0c00d27d9ac719e902cfc242a487d9e589e518
Brayan Medina <bryan@agilityfeat.com>
2020-06-15 13:20:56 -0600

    Add event

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import { trackCustomEvent } from "gatsby-plugin-google-analytics";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const tracking = () => {
+    trackCustomEvent({
+      // string - required - The object that was interacted with (e.g.video)
+      category: "Special Button",
+      // string - required - Type of interaction (e.g. 'play')
+      action: "Click",
+      // string - optional - Useful for categorizing events (e.g. 'Spring Campaign')
+      label: "Gatsby Plugin Example Campaign",
+      // number - optional - Numeric value associated with the event. (e.g. A product ID)
+      value: 43,
+    });
+  };
 
-export default IndexPage
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <button onClick={tracking}>Click here</button>
+    </Layout>
+  );
+};
+
+export default IndexPage;
